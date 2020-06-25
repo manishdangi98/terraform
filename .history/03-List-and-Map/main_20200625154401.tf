221d0643c840464f53f6e37993d5f644d6024fcc
manishdangi98 <manish1998dangi@gmail.com>
2020-06-25 15:49:11 +0530

    iam user name via lis t
    Signed-off-by: manishdangi98 <manish1998dangi@gmail.com>

---
+++ .history/03-List-and-Map/main_20200625154401.tf
@@ -0,0 +1,3 @@
+variable "name"{
+    default= ["manish","guna","saniya"]
+}
---
+++ .history/03-List-and-Map/main_20200625154721.tf
@@ -0,0 +1,12 @@
+variable "names"{
+    default= ["manish","guna","saniya"]
+}
+provider "aws" {
+    region = "us-east-1"
+    version = "~> 2.67"
+}
+
+resource "aws-iam_user" "my_iam_users"{
+    count= length(var.names)
+    name = var.names[count.index]
+}
