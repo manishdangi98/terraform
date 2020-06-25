6b73d168407256fa636e44ebb732d5d2b1186cca
manishdangi98 <manish1998dangi@gmail.com>
2020-06-25 12:48:37 +0530

    3 iam users
    Signed-off-by: manishdangi98 <manish1998dangi@gmail.com>

--- .history/02-more-terraform-basic/main_20200625124338.tf
+++ .history/02-more-terraform-basic/main_20200625124338.tf
@@ -10,6 +10,6 @@ provider "aws" {
 #     }
 # }
 resource "aws_iam_user" "my_iam_users" {
-    count=3
-    name = "my_iam_user_${count.index}"
+    count=2
+    name = "my_iam_users_${count.index}"
 }
